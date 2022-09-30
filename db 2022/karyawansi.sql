f9c2f63a470fb15e3b7a08a01fef66273db0bbf6
hakim-asrori <saneglos005@gmail.com>
2022-09-30 16:33:19 +0700

    first commit

---
+++ db 2022/karyawansi.sql
@@ -0,0 +1,203 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 30 Sep 2022 pada 10.07
+-- Versi server: 10.4.19-MariaDB
+-- Versi PHP: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `karyawansi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_absen`
+--
+
+CREATE TABLE `tb_absen` (
+  `id` int(11) NOT NULL,
+  `id_karyawan` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `waktu` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_absen`
+--
+
+INSERT INTO `tb_absen` (`id`, `id_karyawan`, `nama`, `waktu`) VALUES
+(19, '1', 'euis', 'Wednesday, 24-08-2022 12:04:04 pm');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_daftar`
+--
+
+CREATE TABLE `tb_daftar` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_daftar`
+--
+
+INSERT INTO `tb_daftar` (`id`, `username`, `password`) VALUES
+(2, 'admin', 'admin'),
+(5, 'admin2', 'admin2');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_jabatan`
+--
+
+CREATE TABLE `tb_jabatan` (
+  `id` int(11) NOT NULL,
+  `jabatan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_jabatan`
+--
+
+INSERT INTO `tb_jabatan` (`id`, `jabatan`) VALUES
+(3, 'CEO'),
+(4, 'CTO'),
+(5, 'CFO'),
+(6, 'WP Pemasaran'),
+(7, 'CMO'),
+(8, 'COO'),
+(9, 'Office Boy');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_karyawan`
+--
+
+CREATE TABLE `tb_karyawan` (
+  `id_karyawan` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `tmp_tgl_lahir` varchar(255) NOT NULL,
+  `jenkel` varchar(255) NOT NULL,
+  `agama` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_tel` varchar(18) NOT NULL,
+  `jabatan` varchar(255) NOT NULL,
+  `foto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_karyawan`
+--
+
+INSERT INTO `tb_karyawan` (`id_karyawan`, `username`, `password`, `nama`, `tmp_tgl_lahir`, `jenkel`, `agama`, `alamat`, `no_tel`, `jabatan`, `foto`) VALUES
+(2, 'Aria', '8dd2d051a1c07ddc3bf18179abc222d4', 'Aria Pratama', 'Cirebon, 21 Agustus 2002', 'Laki-laki', 'Islam', 'Gunung Semeru DXI No.74 Perumnas', '081280696486', 'CEO', '25092022155233aria.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_keterangan`
+--
+
+CREATE TABLE `tb_keterangan` (
+  `id` int(11) NOT NULL,
+  `id_karyawan` varchar(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `keterangan` varchar(255) NOT NULL,
+  `alasan` text NOT NULL,
+  `waktu` varchar(255) NOT NULL,
+  `bukti` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_keterangan`
+--
+
+INSERT INTO `tb_keterangan` (`id`, `id_karyawan`, `nama`, `keterangan`, `alasan`, `waktu`, `bukti`) VALUES
+(53, '1', 'euis', 'Sakit', 'sakit', 'Wednesday, 24-08-2022 12:04:14 pm', '24082022070436WhatsApp Image 2022-04-10 at 17.36.2');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_absen`
+--
+ALTER TABLE `tb_absen`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_daftar`
+--
+ALTER TABLE `tb_daftar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_jabatan`
+--
+ALTER TABLE `tb_jabatan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_karyawan`
+--
+ALTER TABLE `tb_karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
+-- Indeks untuk tabel `tb_keterangan`
+--
+ALTER TABLE `tb_keterangan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_absen`
+--
+ALTER TABLE `tb_absen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_daftar`
+--
+ALTER TABLE `tb_daftar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_jabatan`
+--
+ALTER TABLE `tb_jabatan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_keterangan`
+--
+ALTER TABLE `tb_keterangan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
